f97883b3eca5ce461e947d84e29b4b2a24a846a7
alvaroRodriguez92 <alvaros.rodriguez92@gmail.com>
2023-05-31 16:36:51 +0200

    mejora en el nav de characters

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export default function useDebounce(value="", delay:number){
+export default function useDebounce(value:string, delay:number){
     const [debouncedValue, setDebouncedValue] = useState(value);
 
     useEffect(()=>{
